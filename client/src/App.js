d08e210b2c8107c31d701a813f13a55d3f329425
patrick-gordon <patrickgordon42@yahoo.com>
2019-11-25 13:01:07 -0600

    intialized client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import store from './store';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
